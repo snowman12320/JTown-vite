4873e178f5e12bb2928576b72a8402e7930d325f
snowman12320 <snowman12320@gmail.com>
2023-08-13 16:14:36 +0800

    (half progress) updated to composition

--- add.sh
+++ add.sh
@@ -3,11 +3,11 @@ sleep 2
 
 # git commit -m "create github repository    "
 
-git commit -m "update to  composition  API "
+# git commit -m "update to  composition  API "
 
 # git commit -m " fixed  "
 
-# git commit -m "(half progress) updated to composition    "
+git commit -m "(half progress) updated to composition    "
 
 sleep 2
 git push

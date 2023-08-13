749f8ecb80aaedef9b12b3bc87cd27ad90714ace
snowman12320 <snowman12320@gmail.com>
2023-08-13 15:37:56 +0800

    (no composition) vite enviroment done

--- add.sh
+++ add.sh
@@ -3,11 +3,11 @@ sleep 2
 
 # git commit -m "create github repository    "
 
-git commit -m "update to  composition  API "
+# git commit -m "update to  composition  API "
 
 # git commit -m " fixed  "
 
-# git commit -m " (no composition) vite enviroment done   "
+git commit -m " (no composition) vite enviroment done   "
 
 sleep 2
 git push

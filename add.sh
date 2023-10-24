09cb955dd40074b800d530b811297883e13055dc
snowman12320 <snowman12320@gmail.com>
2023-10-24 16:42:15 +0800

    first cypress test

--- add.sh
+++ add.sh
@@ -7,7 +7,7 @@ sleep 2
 
 # git commit -m " fixed suggest2 / git conflict  "
 
-git commit -m "revert note success"
+git commit -m "first cypress test"
 sleep 2
 
 git push
--- cypress/e2e/example.cy.js
+++ cypress/e2e/example.cy.js
@@ -1,8 +1,28 @@
 // https://on.cypress.io/api
+// it('Does not do much!', () => {
+//   expect(true).to.equal(true);
+// });
+// it('visits root url', () => {
+//   cy.visit('http://localhost:5173/');
+// });
 
-describe('My First Test', () => {
-  it('visits the app root url', () => {
-    cy.visit('/')
-    cy.contains('h1', 'You did it!')
-  })
-})
+describe('home view ', () => {
+  it('login', () => {
+    cy.visit('http://localhost:5173/#/login');
+    cy.get('#email').should('exist').click({ force: true }).type('snowman12320@gmail.com');
+    cy.get('#password').should('exist').click({ force: true }).type('BoY4506699');
+
+    cy.get('form > .text-center > .btn').should('exist').click();
+    cy.visit('http://localhost:5173/#/dashboard/members');
+    cy.get('##el-id-7556-17')
+      .find('option')
+      .eq(1)
+      .then(($option) => {
+        cy.wrap($option)
+          .invoke('val')
+          .then((value) => {
+            cy.get('##el-id-7556-17').select(value);
+          });
+      });
+  });
+});
